3d743a9ce29db1a243053442fc863d074393cfac
Sajag-Shrestha <sajagcrestha@gmail.com>
2024-08-23 13:47:50 +0545

    index includes and assests set

---
+++ resources/views/frontend/includes/header.blade.php
@@ -0,0 +1,18 @@
+<head>
+    <title>LuxuryHotel a Hotel Template</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700,900|Rubik:300,400,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('frontend-assets/css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{ asset('frontend-assets/css/animate.css')}}">
+    <link rel="stylesheet" href="{{ asset('frontend-assets/css/owl.carousel.min.css')}}">
+
+    <link rel="stylesheet" href="{{ asset('frontend-assets/fonts/ionicons/css/ionicons.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('frontend-assets/fonts/fontawesome/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('frontend-assets/css/magnific-popup.css')}}">
+
+    <!-- Theme Style -->
+    <link rel="stylesheet" href="{{ asset('frontend-assets/css/style.css')}}">
+  </head>
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,12 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.index');
 });
 
+Route::get('/index', function () {
+    return view('frontend.index');
+});
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
